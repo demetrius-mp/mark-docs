8aa1b2efec246d4ae00be701cb479a312374f2c7
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-01-01 18:23:56 -0400

    refactor: move navbar to its own component

---
+++ src/lib/components/Site/Navbar.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import IconCog from '~icons/mdi/Cog';
+	import IconThemeLightDark from '~icons/mdi/ThemeLightDark';
+	import IconLogoutVariant from '~icons/mdi/LogoutVariant';
+	import { page } from '$app/stores';
+	import { enhance } from '$app/forms';
+	import Form from '$lib/components/Form/Form.svelte';
+	import { themeStore } from '$lib/theme';
+
+	type User = {
+		name: string;
+		profilePictureUrl: string;
+	};
+
+	export let user: User | undefined;
+
+	function handleAfterSubmitToggleTheme() {
+		themeStore.toggle();
+
+		document.documentElement.setAttribute('data-bs-theme', $themeStore);
+	}
+
+	$: signUpLinkIsActive = $page.route.id && $page.route.id === '/sign-up';
+	$: signInLinkIsActive = $page.route.id && $page.route.id === '/sign-in';
+</script>
+
+<nav class="navbar bg-body-secondary">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/app">Mark Docs</a>
+		<ul class="navbar-nav flex-row gap-3">
+			{#if !user}
+				<li class="nav-item">
+					<a class="nav-link" class:active={signUpLinkIsActive} href="/sign-up"> Sign up </a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" class:active={signInLinkIsActive} href="/sign-in"> Sign in </a>
+				</li>
+			{:else}
+				<li class="nav-item dropdown">
+					<button
+						tabindex="0"
+						class="nav-link p-0 border-0"
+						style="background-color: inherit;"
+						data-bs-toggle="dropdown"
+						aria-expanded="false"
+					>
+						<img class="rounded-circle" height="40" src={user.profilePictureUrl} alt="profile" />
+					</button>
+					<ul
+						class="shadow dropdown-menu dropdown-menu-end position-absolute"
+						style="width: 12rem;"
+					>
+						<li>
+							<h6 class="dropdown-header text-wrap">
+								Signed in as
+								<strong>
+									{user.name}
+								</strong>
+							</h6>
+						</li>
+						<li>
+							<hr class="dropdown-divider" />
+						</li>
+						<li>
+							<a class="dropdown-item d-flex align-items-center gap-1" href="/app/settings">
+								<IconCog />
+								Settings
+							</a>
+						</li>
+						<li>
+							<Form action="/theme" method="post" afterSubmit={handleAfterSubmitToggleTheme}>
+								<button type="submit" class="dropdown-item d-flex align-items-center gap-1">
+									<IconThemeLightDark />
+									Toggle theme
+								</button>
+							</Form>
+						</li>
+						<li>
+							<hr class="dropdown-divider" />
+						</li>
+						<li>
+							<form use:enhance action="/sign-out" method="post">
+								<button
+									type="submit"
+									class="dropdown-item text-danger d-flex align-items-center gap-1"
+								>
+									<IconLogoutVariant />
+									Sign out
+								</button>
+							</form>
+						</li>
+					</ul>
+				</li>
+			{/if}
+		</ul>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,35 +2,22 @@
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	import '../app.css';
 	import { onMount } from 'svelte';
-	import IconCog from '~icons/mdi/Cog';
-	import IconThemeLightDark from '~icons/mdi/ThemeLightDark';
-	import IconLogoutVariant from '~icons/mdi/LogoutVariant';
 	import PageLoadingIndicator from '$lib/components/PageLoadingIndicator.svelte';
-	import { navigating, page } from '$app/stores';
-	import { enhance } from '$app/forms';
+	import { navigating } from '$app/stores';
 	import Toasts from '$lib/components/Toasts';
 	import type { LayoutData } from './$types';
-	import Form from '$lib/components/Form/Form.svelte';
 	import { themeStore } from '$lib/theme';
+	import Navbar from '$lib/components/Site/Navbar.svelte';
 
 	export let data: LayoutData;
 
 	themeStore.set(data.theme);
-
-	function handleAfterSubmitToggleTheme() {
-		themeStore.toggle();
-
-		document.documentElement.setAttribute('data-bs-theme', $themeStore);
-	}
 
 	onMount(async () => {
 		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		// @ts-ignore
 		await import('bootstrap/dist/js/bootstrap.bundle');
 	});
-
-	$: signUpLinkIsActive = $page.route.id && $page.route.id === '/sign-up';
-	$: signInLinkIsActive = $page.route.id && $page.route.id === '/sign-in';
 </script>
 
 <PageLoadingIndicator show={Boolean($navigating)} />
@@ -38,82 +25,7 @@
 <Toasts position="bottom-right" />
 
 <header>
-	<nav class="navbar bg-body-secondary">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="/app">Mark Docs</a>
-			<ul class="navbar-nav flex-row gap-3">
-				{#if !data.currentUser}
-					<li class="nav-item">
-						<a class="nav-link" class:active={signUpLinkIsActive} href="/sign-up"> Sign up </a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" class:active={signInLinkIsActive} href="/sign-in"> Sign in </a>
-					</li>
-				{:else}
-					<li class="nav-item dropdown">
-						<button
-							tabindex="0"
-							class="nav-link p-0 border-0"
-							style="background-color: inherit;"
-							data-bs-toggle="dropdown"
-							aria-expanded="false"
-						>
-							<img
-								class="rounded-circle"
-								height="40"
-								src={data.currentUser.profilePictureUrl}
-								alt="profile"
-							/>
-						</button>
-						<ul
-							class="shadow dropdown-menu dropdown-menu-end position-absolute"
-							style="width: 12rem;"
-						>
-							<li>
-								<h6 class="dropdown-header text-wrap">
-									Signed in as
-									<strong>
-										{data.currentUser.name}
-									</strong>
-								</h6>
-							</li>
-							<li>
-								<hr class="dropdown-divider" />
-							</li>
-							<li>
-								<a class="dropdown-item d-flex align-items-center gap-1" href="/app/settings">
-									<IconCog />
-									Settings
-								</a>
-							</li>
-							<li>
-								<Form action="/theme" method="post" afterSubmit={handleAfterSubmitToggleTheme}>
-									<button type="submit" class="dropdown-item d-flex align-items-center gap-1">
-										<IconThemeLightDark />
-										Toggle theme
-									</button>
-								</Form>
-							</li>
-							<li>
-								<hr class="dropdown-divider" />
-							</li>
-							<li>
-								<form use:enhance action="/sign-out" method="post">
-									<button
-										type="submit"
-										class="dropdown-item text-danger d-flex align-items-center gap-1"
-									>
-										<IconLogoutVariant />
-										Sign out
-									</button>
-								</form>
-							</li>
-						</ul>
-					</li>
-				{/if}
-			</ul>
-		</div>
-	</nav>
+	<Navbar user={data.currentUser} />
 </header>
 
 <slot />
